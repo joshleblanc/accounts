e7367394e015a5a5a65b38ca01821293c7b932df
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-04-22 19:53:17 -0300

    add little blurb

--- imports/ui/pages/Application.tsx
+++ imports/ui/pages/Application.tsx
@@ -2,76 +2,86 @@ import PaddedPaper from "meteor/cereal:ui/components/PaddedPaper"
 import * as React from "react";
 import {Field, Form, Formik} from "formik";
 import {TextField} from "formik-material-ui";
-import { useParams } from "react-router-dom";
-import { Meteor } from "meteor/meteor";
+import {useParams} from "react-router-dom";
+import {Meteor} from "meteor/meteor";
 import {useTracker} from "meteor/react-meteor-data";
 import {ApplicationService} from "../../services/ApplicationService";
 import {useSnackbar} from "notistack";
 import StyledButton from "meteor/cereal:ui/components/StyledButton";
-import {Grid} from "@material-ui/core";
+import {Grid, Typography} from "@material-ui/core";
 import {ClientIdField} from "../components/applications/ClientIdField";
 import {ClientSecretField} from "../components/applications/ClientSecretField";
-import { AutoSave } from 'meteor/cereal:ui/components/AutoSave';
+import {AutoSave} from 'meteor/cereal:ui/components/AutoSave';
 
 const applicationService = new ApplicationService();
 
 export const Application = () => {
-  const { enqueueSnackbar } = useSnackbar();
+  const {enqueueSnackbar} = useSnackbar();
   const {id} = useParams();
   Meteor.subscribe('application', id);
   const application = useTracker(() => {
-    if(id) {
+    if (id) {
       return applicationService.findById(id).fetch()[0]
     }
   });
   console.log(application?.clientSecret);
   const handleSubmit = React.useCallback((values) => {
     Meteor.call('applications.update', id, values.name, (err?: Meteor.Error) => {
-      if(err) {
-        enqueueSnackbar("Error updating application", { variant: "error" });
+      if (err) {
+        enqueueSnackbar("Error updating application", {variant: "error"});
       } else {
-        enqueueSnackbar("Application updated", { variant: "success"});
+        enqueueSnackbar("Application updated", {variant: "success"});
       }
     })
   }, [id]);
-  if(!id || !application) return null;
+  if (!id || !application) return null;
 
-  return(
-    <PaddedPaper>
-      <Formik onSubmit={handleSubmit} initialValues={application} enableReinitialize>
-        {
-          ({isSubmitting, values}) => {
-            return (
-              <Form>
-                <Field
-                  name={"name"}
-                  label={"Name"}
-                  component={TextField}
-                  fullWidth
-                  margin={"normal"}
-                />
-                <Grid container spacing={2}>
-                  <Grid item xs={12} md={6}>
-                    <ClientIdField/>
-                  </Grid>
-                  <Grid item xs={12} md={6}>
-                    <ClientSecretField/>
-                  </Grid>
-                </Grid>
-                <StyledButton
-                  type={"submit"}
-                  loading={isSubmitting}
-                  variant={"contained"}
-                  color={"primary"}
-                >
-                  Submit
-                </StyledButton>
-                <AutoSave debounce={1000} onSave={handleSubmit} values={values} />
-              </Form>
-            )
-          }
-        }
-      </Formik>
-    </PaddedPaper>
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <PaddedPaper>
+          <Typography variant={"h4"}>General Information</Typography>
+          <Typography variant={"h6"}>Create your application here</Typography>
+        </PaddedPaper>
+      </Grid>
+      <Grid item xs={12}>
+        <PaddedPaper>
+          <Formik onSubmit={handleSubmit} initialValues={application} enableReinitialize>
+            {
+              ({isSubmitting, values}) => {
+                return (
+                  <Form>
+                    <Field
+                      name={"name"}
+                      label={"Name"}
+                      component={TextField}
+                      fullWidth
+                      margin={"normal"}
+                    />
+                    <Grid container spacing={2}>
+                      <Grid item xs={12} md={6}>
+                        <ClientIdField/>
+                      </Grid>
+                      <Grid item xs={12} md={6}>
+                        <ClientSecretField/>
+                      </Grid>
+                    </Grid>
+                    <StyledButton
+                      type={"submit"}
+                      loading={isSubmitting}
+                      variant={"contained"}
+                      color={"primary"}
+                    >
+                      Submit
+                    </StyledButton>
+                    <AutoSave debounce={1000} onSave={handleSubmit} values={values}/>
+                  </Form>
+                )
+              }
+            }
+          </Formik>
+        </PaddedPaper>
+      </Grid>
+    </Grid>
   )
 }
