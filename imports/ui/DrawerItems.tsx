bec50d74688d3ba9eed4e24c5708daf3792eb0cd
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-04-22 20:22:30 -0300

    add profile link

--- imports/ui/DrawerItems.tsx
+++ imports/ui/DrawerItems.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
-import {List} from "@material-ui/core";
+import {Divider, List, ListItem, ListItemText} from "@material-ui/core";
 import {AdminDrawerItems} from "/imports/ui/components/drawer_items/AdminDrawerItems";
+import {Link} from "react-router-dom";
 
 export const DrawerItems = () => {
   return(
     <List>
+      <ListItem button component={Link} to={"/profile"}>
+        <ListItemText primary={"Profile"} />
+      </ListItem>
+      <Divider />
       <AdminDrawerItems />
     </List>
   )
