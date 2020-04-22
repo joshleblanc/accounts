e77c2eadd14ded6710bb7ceef5df968c10852a87
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-04-22 15:10:54 -0300

    hide client secret by default

--- imports/ui/components/applications/ClientSecretField.tsx
+++ imports/ui/components/applications/ClientSecretField.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Field} from "formik";
 import {TextField} from "formik-material-ui";
+import MuiTextField from '@material-ui/core/TextField'
 import {useSnackbar} from "notistack";
 import StyledButton from 'meteor/cereal:ui/components/StyledButton';
 import {makeStyles} from "@material-ui/core/styles";
@@ -16,6 +17,7 @@ const useStyles = makeStyles(theme => {
 export const ClientSecretField = () => {
   const { enqueueSnackbar } = useSnackbar();
   const classes = useStyles();
+  const [fieldVisible, setFieldVisible] = React.useState(false);
   const inputRef = React.useRef(null);
   const handleClick = React.useCallback((e) => {
     const target = inputRef.current;
@@ -26,20 +28,35 @@ export const ClientSecretField = () => {
     e.target.focus();
     enqueueSnackbar("Client secret copied!", { variant: "info" });
   }, []);
+  const showField = React.useCallback(() => {
+    setFieldVisible(true);
+  }, [setFieldVisible]);
   return(
     <>
-      <Field
-        name={"clientSecret"}
-        label={"Client Secret"}
-        component={TextField}
-        fullWidth
-        InputProps={{
-          readOnly:true
-        }}
-        variant={"outlined"}
-        margin={"normal"}
-        inputRef={inputRef}
-      />
+      {
+        fieldVisible ? <Field
+          name={"clientSecret"}
+          label={"Client Secret"}
+          component={TextField}
+          fullWidth
+          InputProps={{
+            readOnly:true
+          }}
+          variant={"outlined"}
+          margin={"normal"}
+          inputRef={inputRef}
+        /> : <MuiTextField
+          value={"Click to reveal"}
+          label={"Client Secret"}
+          fullWidth
+          InputProps={{
+            readOnly: true
+          }}
+          onClick={showField}
+          margin={"normal"}
+          variant={"outlined"}
+        />
+      }
       <StyledButton
         loading={false}
         onClick={handleClick}
